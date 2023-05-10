0bc6ee7598327dd54fbe57230a7ea25433625f34
RaulLoyola <raul.loy@gmail.com>
2023-05-09 22:43:46 -0600

    Create React App

---
+++ frontend/src/App.js
@@ -0,0 +1,14 @@
+import './App.css';
+
+function App() {
+  return (
+    <div>
+      <header>
+        <a href="/">Amazon</a>
+      </header>
+      <main>List of products</main>
+    </div>
+  );
+}
+
+export default App;
