bbc467a11a05b91c032bdf4b96edaef155e03d76
RaulLoyola <raul.loy@gmail.com>
2023-05-11 20:58:43 -0600

    Creating Node.js Server

---
+++ backend/data.js
@@ -0,0 +1,84 @@
+const data = {
+  products: [
+    {
+      name: 'Apple MacBook Pro',
+      slug: 'apple-macbook-pro',
+      category: 'Electronics',
+      image: '/images/Apple MacBook Pro.jpg',
+      price: 1299,
+      countInStock: 8,
+      brand: 'Apple',
+      rating: 4.8,
+      numReviews: 15,
+      description:
+        'The Apple MacBook Pro is a powerful and versatile laptop, perfect for professionals and creatives.',
+    },
+    {
+      name: 'Samsung Galaxy S21',
+      slug: 'samsung-galaxy-s21',
+      category: 'Electronics',
+      image: '/images/Samsung Galaxy S21.jpg',
+      price: 799,
+      countInStock: 12,
+      brand: 'Samsung',
+      rating: 4.6,
+      numReviews: 18,
+      description:
+        'The Samsung Galaxy S21 is a top-of-the-line smartphone with cutting-edge features and performance.',
+    },
+    {
+      name: 'Bose QuietComfort 35 II',
+      slug: 'bose-quietcomfort-35-ii',
+      category: 'Electronics',
+      image: '/images/Bose QuietComfort 35 II.jpg',
+      price: 299,
+      countInStock: 6,
+      brand: 'Bose',
+      rating: 4.5,
+      numReviews: 11,
+      description:
+        'The Bose QuietComfort 35 II is a premium noise-cancelling headphone that delivers exceptional sound quality and comfort.',
+    },
+    {
+      name: 'Nespresso VertuoPlus Coffee Maker',
+      slug: 'nespresso-vertuoplus-coffee-maker',
+      category: 'Home',
+      image: '/images/Nespresso VertuoPlus Coffee Maker.jpg',
+      price: 149,
+      countInStock: 9,
+      brand: 'Nespresso',
+      rating: 4.3,
+      numReviews: 7,
+      description:
+        'The Nespresso VertuoPlus Coffee Maker is a stylish and easy-to-use appliance that makes delicious coffee with just the touch of a button.',
+    },
+    {
+      name: 'iRobot Roomba 960',
+      slug: 'irobot-roomba-960',
+      category: 'Home',
+      image: '/images/iRobot Roomba 960.jpg',
+      price: 499,
+      countInStock: 3,
+      brand: 'iRobot',
+      rating: 4.7,
+      numReviews: 21,
+      description:
+        'The iRobot Roomba 960 is a high-performance robotic vacuum cleaner that cleans your floors with precision and ease.',
+    },
+    {
+      name: 'Instant Pot Duo Nova Pressure Cooker',
+      slug: 'instant-pot-duo-nova-pressure-cooker',
+      category: 'Home',
+      image: '/images/Instant Pot Duo Nova Pressure Cooker.jpg',
+      price: 119,
+      countInStock: 11,
+      brand: 'Instant Pot',
+      rating: 4.4,
+      numReviews: 13,
+      description:
+        'The Instant Pot Duo Nova Pressure Cooker is a versatile and convenient kitchen appliance that lets you cook meals quickly and easily.',
+    },
+  ],
+};
+
+export default data;
