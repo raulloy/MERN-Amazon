d647590de619db48d2ac07d18d8e3cf4bf9b419c
RaulLoyola <raul.loy@gmail.com>
2023-05-19 20:18:42 -0600

    Creating User Model

--- backend/data.js
+++ backend/data.js
@@ -1,4 +1,20 @@
+import bcrypt from 'bcryptjs';
+
 const data = {
+  users: [
+    {
+      name: 'Raul Loyola',
+      email: 'raul.loy@gmail.com',
+      password: bcrypt.hashSync('123456'),
+      isAdmin: true,
+    },
+    {
+      name: 'User',
+      email: 'user@example.com',
+      password: bcrypt.hashSync('123456'),
+      isAdmin: false,
+    },
+  ],
   products: [
     {
       name: 'Apple MacBook Pro',
